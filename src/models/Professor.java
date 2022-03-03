73d27331ab804190e64c72ad56f4967c16c36721
Karina Skirmante <karina.krinkele@venta.lv>
2022-03-03 15:58:15 +0200

    Edit: professor variables

--- src/models/Professor.java
+++ src/models/Professor.java
@@ -1,5 +1,15 @@
 package models;
 
 public class Professor {
-
+//1.mainīgie
+	private String name;
+	private String surname;
+	private ProfDegree degree;
+//2.get funkcijas
+//3.set funkcijas
+//4. konstruktori
+//5. toString funkcija
+//6. - pārējās funkcijas
+	
+	
 }
