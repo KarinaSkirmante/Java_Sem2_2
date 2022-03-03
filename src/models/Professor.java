648c286df3126223a48d88cc66e44e6300d61743
Karina Skirmante <karina.krinkele@venta.lv>
2022-03-03 16:01:34 +0200

    Edit: professor get functions

--- src/models/Professor.java
+++ src/models/Professor.java
@@ -5,7 +5,20 @@ public class Professor {
 	private String name;
 	private String surname;
 	private ProfDegree degree;
+	
 //2.get funkcijas
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public ProfDegree getDegree() {
+		return degree;
+	}
+	
+	
+	
 //3.set funkcijas
 //4. konstruktori
 //5. toString funkcija
