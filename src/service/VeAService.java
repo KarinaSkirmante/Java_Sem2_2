971261be5a62f4fc723d0f821bf78c6c8d00134d
Karina Skirmante <karina.krinkele@venta.lv>
2022-03-10 16:11:19 +0200

    Edit: VeAService with Student, Grade objects

--- src/service/VeAService.java
+++ src/service/VeAService.java
@@ -3,8 +3,10 @@ package service;
 import java.util.Random;
 
 import models.Course;
+import models.Grade;
 import models.ProfDegree;
 import models.Professor;
+import models.Student;
 
 
 
@@ -29,7 +31,20 @@ public class VeAService {
 		System.out.println(c2);
 		System.out.println(c3);
 		System.out.println(c4);
-
+		
+		Student stud1 = new Student("Jānis", "Bērziņš");
+		Student stud2 = new Student("Līga", "Jaukā");
+		Student stud3 = new Student("Baiba", "Nejaukā");
+		System.out.println(stud1);
+		System.out.println(stud2);
+		System.out.println(stud3);
+		
+		Grade gr1 = new Grade(10, stud1, c1);//Jānis, JAVA, 10
+		Grade gr2 = new Grade(5, stud1, c4);//Jānis, DatuStr, 5
+		Grade gr3 = new Grade(3, stud2, c1);//Līga, JAVA, 3
+		System.out.println(gr1);
+		System.out.println(gr2);
+		System.out.println(gr3);
 
 	}
 
