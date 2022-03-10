786b629ed4163accf56684716f7e95f6fd6f8309
Karina Skirmante <karina.krinkele@venta.lv>
2022-03-10 15:51:12 +0200

    Add: Grade class with variables

---
+++ src/models/Grade.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Grade {
+	//1.mainīgie
+	private int id;
+	private int gradeValue;
+	private Student student;
+	private Course course;
+	
+	private static int idCounter = 1000000;
+	
+
+}
