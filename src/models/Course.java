9f98133df1322beee02f16f18fde50b5cdb2fd93
Karina Skirmante <karina.krinkele@venta.lv>
2022-03-10 15:07:10 +0200

    Edit: Cource class set and get

--- src/models/Course.java
+++ src/models/Course.java
@@ -9,4 +9,66 @@ public class Course {
 	
 	private static int idCounter = 10000;
 
+	//2.1 get funkcijas
+	public int getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public byte getCreditPoints() {
+		return creditPoints;
+	}
+
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setId() {
+		this.id = idCounter++;
+	}
+
+	public void setTitle(String title) {
+		if(title!=null && title.matches("[A-ZĒŪĪĀŠĢĶĻŅČŽ]{1}[\\s\\da-zēūīļķģšāžčņA-ZĒŪĪĀŠĢĶĻŅČŽ]+"))
+		{
+			this.title = title;
+		}
+		else
+		{
+			this.title= "unknown";
+		}
+	}
+
+	public void setCreditPoints(byte creditPoints) {
+		if(creditPoints>0 && creditPoints<=20)
+		{
+			this.creditPoints = creditPoints;
+		}
+		else
+		{
+			this.creditPoints = 2;
+		}
+	}
+
+	public void setProfessor(Professor professor) {
+		if(professor!=null)
+		{
+			this.professor = professor;
+		}
+		else
+		{
+			this.professor = new Professor();
+		}
+	}
+	
+	//2.2. set funkcijas
+	
+	
+	
+	//3.konstruktori
+	//4.toString
+	
+
 }
