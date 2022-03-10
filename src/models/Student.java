6da991729b5923222c41b72bb41e7789e1d8e2f3
Karina Skirmante <karina.krinkele@venta.lv>
2022-03-10 15:46:04 +0200

    Edit: Student get, set , consstructors, toString

--- src/models/Student.java
+++ src/models/Student.java
@@ -7,7 +7,64 @@ public class Student {
 	private String surname;
 	
 	private static int idCounter = 100000;
+	//2.1. get
+	public int getId() {
+		return id;
+	}
 	
+	public String getName() {
+		return name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+	//2.2. set
+	public void setId()
+	{
+		this.id = idCounter;
+		idCounter++;
+	}
+	
+	public void setName(String name) {
+		if(name!=null && name.matches("[A-ZĒŪĪĀŠĢĶĻŅČŽ]{1}[a-zēūīļķģšāžčņ]+\\s?([A-ZĒŪĪĀŠĢĶĻŅČŽ]{1}[a-zēūīļķģšāžčņ]+)?"))
+		{
+			this.name = name;
+		}
+		else
+		{
+			this.name = "notknown";
+		}
+	}
+	public void setSurname(String surname) {
+		if(surname!=null && surname.matches("[A-ZĒŪĪĀŠĢĶĻŅČŽ]{1}[a-zēūīļķģšāžčņ]+[-]?([A-ZĒŪĪĀŠĢĶĻŅČŽ]{1}[a-zēūīļķģšāžčņ]+)?"))
+		{
+			this.surname = surname;
+		}
+		else
+		{
+			this.surname = "notknown";
+		}
+	}
+	//3. konstruktori
+	public Student()
+	{
+		setId();
+		setName("Test");
+		setSurname("Student");
+	}
+	
+	public Student(String name, String surname)
+	{
+		setId();
+		setName(name);
+		setSurname(surname);
+	}
+	//4.toString
+	public String toString()
+	{
+		return id+ " "+name + " " + surname;
+	}
 	
 
 }
